133598c7aa65f959c60468c37805c200536491b3
Shinya Fujino <shf0811@gmail.com>
2020-04-18 06:17:45 +0900

    Update repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/morinokami/go.calc/lexer"
-	"github.com/morinokami/go.calc/token"
+	"github.com/morinokami/go.calc/parser"
 )
 
 const PROMPT = "> "
@@ -23,9 +23,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.Parse()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
